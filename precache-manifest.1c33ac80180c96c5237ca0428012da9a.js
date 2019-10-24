852a6500da00a0635e1285a80ad4db71a6c220f3
alimam15 <46492827+alimam15@users.noreply.github.com>
2019-10-24 13:39:17 +0100

    Updates

---
+++ precache-manifest.1c33ac80180c96c5237ca0428012da9a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c512f044a6e9d2e602e12d2224dfbbeb",
+    "url": "/alimam15/React-Tests.git/index.html"
+  },
+  {
+    "revision": "2bc34f3c07248ff472aa",
+    "url": "/alimam15/React-Tests.git/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "0dff2eefff597de00f89",
+    "url": "/alimam15/React-Tests.git/static/css/main.7255cc43.chunk.css"
+  },
+  {
+    "revision": "2bc34f3c07248ff472aa",
+    "url": "/alimam15/React-Tests.git/static/js/2.f3903a4a.chunk.js"
+  },
+  {
+    "revision": "0dff2eefff597de00f89",
+    "url": "/alimam15/React-Tests.git/static/js/main.cd9fa450.chunk.js"
+  },
+  {
+    "revision": "9b024053d604dd0a275c",
+    "url": "/alimam15/React-Tests.git/static/js/runtime-main.63adca1a.js"
+  },
+  {
+    "revision": "1644f7a9d958d180800e21f10bd6e73b",
+    "url": "/alimam15/React-Tests.git/static/media/zombie1.1644f7a9.jpg"
+  },
+  {
+    "revision": "ff80d5c0267031b80c336041389022c2",
+    "url": "/alimam15/React-Tests.git/static/media/zombie10.ff80d5c0.jpg"
+  },
+  {
+    "revision": "3ab009d855cde2841d0cddd11348235d",
+    "url": "/alimam15/React-Tests.git/static/media/zombie2.3ab009d8.jpg"
+  },
+  {
+    "revision": "402f4d9374a8eb9349e24f0757263178",
+    "url": "/alimam15/React-Tests.git/static/media/zombie3.402f4d93.jpg"
+  },
+  {
+    "revision": "b006dc9db5f0aea6fcdc94edce0d5529",
+    "url": "/alimam15/React-Tests.git/static/media/zombie4.b006dc9d.jpg"
+  },
+  {
+    "revision": "7a27d7124e6aeaab2c350d991bdae3da",
+    "url": "/alimam15/React-Tests.git/static/media/zombie5.7a27d712.jpg"
+  },
+  {
+    "revision": "12a6e3ac66242718d0d00559dc134961",
+    "url": "/alimam15/React-Tests.git/static/media/zombie6.12a6e3ac.jpg"
+  },
+  {
+    "revision": "9add9c71d4d09ef4c9e6c417ddb3843c",
+    "url": "/alimam15/React-Tests.git/static/media/zombie7.9add9c71.jpg"
+  },
+  {
+    "revision": "20b5e908d17a64bf2e48851bba4cb4da",
+    "url": "/alimam15/React-Tests.git/static/media/zombie8.20b5e908.jpg"
+  },
+  {
+    "revision": "f0ea1e439fd2b8c991dc3cc33a127ee8",
+    "url": "/alimam15/React-Tests.git/static/media/zombie9.f0ea1e43.jpg"
+  }
+]);
